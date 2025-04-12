fa3a28aac393e0999ed46861134747eb6936c69a
LegalQuizzes <newages33@yahoo.com>
2025-04-12 16:52:54 +0300

    questions.js

--- questions.js
+++ questions.js
@@ -25,5 +25,23 @@ const questions = [
       { text: "Nu vreau 500 lei", correct: false },
       { text: "Vreau mai mult de 500 lei", correct: false }
     ]
+  },
+{
+    id: "q4",
+    text: "Este posibil sa vi se faca foame?",
+    answers: [
+      { text: "Da, am muncit azi", correct: true },
+      { text: "Nuuuu,, niciodata zilnic", correct: false },
+      { text: "Nuuuu,, niciodata lunar", correct: false }
+    ]
+  },
+{
+    id: "q5",
+    text: "Cum ati tras la jug?",
+    answers: [
+      { text: "La misto", correct: true },
+      { text: "Din greu", correct: false },
+      { text: "Mai putin", correct: false }
+    ]
   }
 ];
