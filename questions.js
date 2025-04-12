0a1711bee18938ded453bba338dd25a4b7caa3fe
LegalQuizzes <newages33@yahoo.com>
2025-04-12 16:12:13 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,30 @@
+
+const questions = [
+  {
+    id: "q1",
+    text: "Ce este JavaScript?",
+    answers: [
+      { text: "Limbaj de stilizare", correct: false },
+      { text: "Limbaj de programare", correct: true },
+      { text: "Sistem de operare", correct: false }
+    ]
+  },
+  {
+    id: "q2",
+    text: "Cât face 9 x 3?",
+    answers: [
+      { text: "27", correct: true },
+      { text: "21", correct: false },
+      { text: "36", correct: false }
+    ]
+  },
+  {
+    id: "q3",
+    text: "Ce folosești pentru a crea structura unei pagini web?",
+    answers: [
+      { text: "HTML", correct: true },
+      { text: "CSS", correct: false },
+      { text: "Photoshop", correct: false }
+    ]
+  }
+];
